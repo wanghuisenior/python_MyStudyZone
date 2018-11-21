5fdd68d2b6d009d210e1783187270e77bdd56d96
wanghui <154306777@qq.com>
2018-11-21 09:32:33 +0800

    Revert "修改配置文件"
    This reverts commit f8eb905d1f63b1a808810a587cfd1111da971daf.

--- MyStudyZone/MyStudyZone/settings.py
+++ MyStudyZone/MyStudyZone/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mystudyzone',  # 数据库名字
+        'NAME': 'aaa',  # 数据库名字
         'USER': 'root',
         'PASSWORD': 'root',
         # 'HOST': '192.168.16.198',
