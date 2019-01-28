ec0587ac434454cedbda717d683a6c30331ddf9c
wanghui <154306777@qq.com>
2019-01-28 16:01:04 +0800

    哎呦喂

---
+++ MyStudyZone/test_worm/worms/tkinter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ @FileName: tkinter.py
+ @Author: 王辉/Administrator
+ @Email: wanghui@zih718.com
+ @Date: 2019/1/25 15:56
+ @Description:
+"""
+import tkinter
+
+top = tkinter.Tk()
+# 进入消息循环
+top.mainloop()
